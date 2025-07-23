37399aeb8c4bc261eef1c04e7c0851e76b2382a2
priyanshuthakran1 <priyanshuthakran9410@gmail.com>
2025-07-23 21:52:28 +0530

    Local changes before pulling

--- Backend/conn/conn.js
+++ Backend/conn/conn.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const conn = async() => {
     try {
-        await mongoose.connect("mongodb+srv://priyanshuthakran1235:12345678900987654321@cluster1.veik2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1").then(() => {
+        await mongoose.connect("mongodb+srv://awalmanga1234:priyanshuuser1@cluster1.wb6nm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1").then(() => {
             console.log("Connected");
+            // priyanshuthakran1235:12345678900987654321
         });
     } catch (error) {
         console.error("Not Connected", error);
